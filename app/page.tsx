a375ef0f57e23d6a03e1e5092ad13b2418c94555
mlambow <wandile.mlambo@yahoo.com>
2025-12-01 19:11:43 +0200

    Updated Landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default async function LandingPage() {
             </p>
 
             <SignUpButton>
-                <Button className='mx-auto mt-12 md:mt-10 text-3xl flex items-center justify-center dark:bg-primary/90 py-8 rounded-full cursor-pointer font-light dark:hover:bg-primary/40 hover:bg-secondary-foreground' size='lg'>
+                <Button className='mx-auto mt-12 md:mt-10 text-3xl flex items-center justify-center dark:bg-primary/90 py-8 rounded-full cursor-pointer font-light dark:hover:bg-secondary-foreground dark:hover:text-black hover:bg-secondary-foreground' size='lg'>
                     <div className='flex items-center justify-center font-semibold'>
                         Start Training
                         <ArrowRight className='ml-4 mt-1 size-10' />
